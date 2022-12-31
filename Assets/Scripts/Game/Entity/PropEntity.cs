9e819451e1529080f370ca4195276454d2c62bd4
YPZhou <yunpeng_zhou@163.com>
2022-12-31 16:47:42 +0800

    调整跑道颜色

--- Assets/Scripts/Game/Entity/PropEntity.cs
+++ Assets/Scripts/Game/Entity/PropEntity.cs
@@ -2,12 +2,27 @@ using UnityEngine;
 
 namespace GGJ2023.Beta
 {
-    /// <summary>
-    /// 道具实体基类。
-    /// </summary>
-    public class PropEntity : MonoBehaviour
-    {
-        [SerializeField]
-        public ColorType colorType;
-    }
+	/// <summary>
+	/// 道具实体基类。
+	/// </summary>
+	public class PropEntity : MonoBehaviour
+	{
+		void Start()
+		{
+			if (colorType == ColorType.Red)
+			{
+				sprite.color = Color.red;
+			}
+			else if (colorType == ColorType.Blue)
+			{
+				sprite.color = Color.blue;
+			}
+		}
+
+		[SerializeField]
+		SpriteRenderer sprite;
+
+		[SerializeField]
+		public ColorType colorType;
+	}
 }
