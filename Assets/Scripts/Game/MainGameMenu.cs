757d019243b9e835f336ca953746847b136d9d13
YPZhou <yunpeng_zhou@163.com>
2022-12-31 14:30:04 +0800

    完成游戏主菜单操作逻辑

--- Assets/Scripts/Game/MainGameMenu.cs
+++ Assets/Scripts/Game/MainGameMenu.cs
@@ -8,25 +8,83 @@ namespace GGJ2023.Beta
 		// Start is called before the first frame update
 		void Start()
 		{
-
+			GameStatus.IsGameRunning = true;
 		}
 
 		// Update is called once per frame
 		void Update()
 		{
+			if (GameStatus.IsGameRunning)
+			{
+				mainGameMenu.gameObject.SetActive(false);
 
+				if (Input.GetKeyUp(KeyCode.Escape))
+				{
+					GameStatus.IsGameRunning = false;
+				}
+			}
+			else
+			{
+				mainGameMenu.gameObject.SetActive(true);
+
+				for (var i = 0; i < gameMenuTextList.Length; i++)
+				{
+					var gameMenuText = gameMenuTextList[i];
+					if (currentGameMenu == i)
+					{
+						gameMenuText.color = Color.red;
+					}
+					else
+					{
+						gameMenuText.color = Color.white;
+					}
+				}
+
+				if (Input.GetKeyUp(KeyCode.UpArrow))
+				{
+					currentGameMenu -= 1;
+					if (currentGameMenu < 0)
+					{
+						currentGameMenu = 0;
+					}
+				}
+
+				if (Input.GetKeyUp(KeyCode.DownArrow))
+				{
+					currentGameMenu += 1;
+					if (currentGameMenu > 2)
+					{
+						currentGameMenu = 2;
+					}
+				}
+
+				if (Input.GetKeyUp(KeyCode.Return))
+				{
+					switch (currentGameMenu)
+					{
+						case 0:	// resume game
+							GameStatus.IsGameRunning = true;
+							break;
+						case 1: // start new game
+							GameStatus.IsGameRunning = true;
+							break;
+						case 2: // quit game
+#if UNITY_EDITOR
+							UnityEditor.EditorApplication.isPlaying = false;
+#endif
+							Application.Quit();
+							break;
+					}
+				}
+			}
 		}
 
 		[SerializeField]
-		GameObject mainGameMenu;
+		Image mainGameMenu;
 
 		[SerializeField]
-		Text resumeGameText;
+		Text[] gameMenuTextList;
 
-		[SerializeField]
-		Text startNewGameText;
-
-		[SerializeField]
-		Text endGameText;
+		int currentGameMenu;
 	}
 }
