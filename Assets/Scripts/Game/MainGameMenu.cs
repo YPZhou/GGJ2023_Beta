8a99cd18e008b0ecc9b80ff5a9ee15c4b7ef1ca4
YPZhou <yunpeng_zhou@163.com>
2022-12-31 14:08:42 +0800

    添加游戏主菜单

---
+++ Assets/Scripts/Game/MainGameMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ2023.Beta
+{
+	public class MainGameMenu : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		[SerializeField]
+		GameObject mainGameMenu;
+
+		[SerializeField]
+		Text resumeGameText;
+
+		[SerializeField]
+		Text startNewGameText;
+
+		[SerializeField]
+		Text endGameText;
+	}
+}
